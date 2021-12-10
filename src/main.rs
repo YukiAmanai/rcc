bc89b6075df7b4af5f122a392078948b66e6353d
YukiAmanai <yuki_amanai@icloud.com>
2021-12-10 14:21:08 +0900

    整数一個をコンパイルする言語作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    if args.len() != 2  {
+        eprint!("引数の個数が正しくありません\n");
+        return;
+    }
+    println!(".intel_syntax noprefix\n");
+    println!(".globl main\n");
+    println!("main:\n");
+    println!("  mov rax, %d{}\n",args.nth(1).unwrap());
+    println!("  ret\n");
+    return;
 }
