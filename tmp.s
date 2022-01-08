98055e2a499b393ae925cd9c2415d37c7644c732
YukiAmanai <yuki_amanai@icloud.com>
2022-01-08 23:37:25 +0900

    READMEに追記

--- tmp.s
+++ tmp.s
@@ -1,10 +1,10 @@
 .intel_syntax noprefix
 .global main
 main:
-  push 12  pop rdi
+  push 5  pop rdi
   pop rax
-  push rax  push 12  pop rdi
+  push rax  push 5  pop rdi
   pop rax
   push rax  pop rdi
   pop rax
-  add rax, rdi  pop rax  ret
+ imul rax, rdi  pop rax  ret
